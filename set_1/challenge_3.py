f4b44ef2fae995770b8ec8b24608462713650e62
Ihab Bokal <bokalihab@gmail.com>
2023-11-27 17:12:07 +0100

    Put the solving challenge 3 to a halt until I learn z3 and crypto.utils

--- set_1/challenge_3.py
+++ set_1/challenge_3.py
@@ -1,6 +1,4 @@
-from codecs import decode
-from challenge_2 import fixed_xor
-hex_digits = "0123456789abcdef"
+english_letters = "eariotnslcudpmhgbfywkvxzjq"
 
 
 def frequency(string: str):
@@ -10,33 +8,19 @@ def frequency(string: str):
         # Increment the count of the encountered letter
         hex_dict[string[i]] += 1
     print(hex_dict)
+    return hex_dict
 
 
-def decipher(string: str):
-    for i in range(len(hex_digits)):
-        x = decode(fixed_xor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", hex_digits[i] * 68), "hex")
-        print(x)
-
-"""
-from codecs import decode
-
-most_frequent_leters = "eariotnslcudpmhgbfywkvxzjq"
-frequent_digits = "3781b269df45abc"
-
-def frequency(string: str):
-    hex_dict = {"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "a": 0, "b": 0, "c": 0,
-                "d": 0, "e": 0, "f": 0}
-    for i in range(len(string)):
-        # Increment the count of the encountered letter
-        hex_dict[string[i]] += 1
-    print(decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", "hex"))
-    print(hex_dict)
-
-
-def decipher(string: str):
-    for 
-    return 0
-
-
-frequency("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-"""
+def decipher(ciphertext: str):
+    # This list is going to contain a list of characters
+    result = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+              'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+              'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+              'x', 'x', 'x', 'x', 'x']
+    cipher_dict = frequency(ciphertext)
+    j = 0
+    maxvalue = max(cipher_dict, key=cipher_dict.get)
+    while maxvalue != 0:
+        for i in range(len(ciphertext)):
+            if ciphertext[i] == 0:
+                result[i] = english_letters[j]
