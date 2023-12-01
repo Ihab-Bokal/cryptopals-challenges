2015c825e975ba3dfbea6028b99624adf5bb2256
Ihab Bokal <bokalihab@gmail.com>
2023-12-01 18:17:55 +0100

    Make some advancements in understanding the problem and how to solve it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-x = list()
-for i in range(68):
-    x.append("x")
+from set_1.challenge_2 import *
 
+
+hex_digits = "0123456789abcdef"
+
+x = hexadecimal_to_binary("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 print(x)
