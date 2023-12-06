22751200eb87cd558d90d3d521c467ef94ea5a16
Ihab Bokal <bokalihab@gmail.com>
2023-12-06 03:26:30 +0100

    Solved challenge 3

--- set_1/chal2_solution.py
+++ set_1/chal2_solution.py
@@ -4,7 +4,7 @@ buffer1 = decode(b"1c0111001f010100061a024b53535009181c", "hex_codec")
 buffer2 = decode(b"686974207468652062756c6c277320657965", "hex_codec")
 
 
-def xor_buffers(buf1, buf2):
+def xor_buffers(buf1: bytes, buf2: bytes) -> bytes:
     result = [x ^ y for x, y in zip(buf1, buf2)]
     return bytes(result)
 
---
+++ set_1/challenge3_solution.py
@@ -0,0 +1,50 @@
+from collections import Counter
+from string import ascii_lowercase, ascii_letters
+from chal2_solution import xor_buffers
+from codecs import decode
+
+with open("frankenstein.txt", encoding='utf-8') as f:
+    # have the book all in lowercase
+    book = f.read().lower()
+
+buffer = decode(b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", "hex_codec")
+
+
+# A method to get the frequency of each character in the frankenstein book
+def get_freqs(txt: str, letters: str) -> dict:
+    counts = Counter()
+    for letter in letters:
+        counts[letter] += txt.count(letter)
+    total = sum(counts.values())
+    # Return a dictionary of key values
+    return {letter: counts[letter] / total for letter in letters}
+
+
+# The reference dict that we'll use to score our plaintext
+letter_freq = get_freqs(book, ascii_lowercase)
+
+
+def scorer(plaintext: str) -> float:
+    score = 0.0
+    for letter in plaintext:
+        try:
+            # Sum up the values to each letter to get a final score
+            score += letter_freq[letter]
+        except KeyError:
+            continue
+    return score
+
+
+# We decode using only the letter characters from the string library
+def decoder(ciphertext: bytes) -> str:
+    result = ""
+    for letter in ascii_letters:
+        # Generate a 68 byte long key cast to bytes
+        key = bytes(letter * 68, 'utf-8')
+        if scorer(str(xor_buffers(key, ciphertext))) > 1.2:
+            result = str(xor_buffers(key, ciphertext))
+            print(letter, ": ", result)
+    return result
+
+
+print(decoder(buffer))
