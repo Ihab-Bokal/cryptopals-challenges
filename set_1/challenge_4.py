0cd89d42a6cb1e7d070c4aa445426378542b47c9
Ihab Bokal <bokalihab@gmail.com>
2023-12-11 15:29:34 +0100

    Final try with chal4
    This is my final try with challenge 4, couldn't solve it tbh but I think I was pretty close.

--- set_1/challenge_4.py
+++ set_1/challenge_4.py
@@ -1,6 +1,7 @@
-from string import ascii_letters
+from string import ascii_uppercase
 from challenge3_solution import scorer
 from challenge2_solution import xor_buffers
+from codecs import decode
 
 with open("challenge4_ciphertext.txt", encoding='utf-8') as c:
     ciphertext = c.read()
@@ -8,27 +9,41 @@ with open("challenge4_ciphertext.txt", encoding='utf-8') as c:
 
 # The full buffer's length is 333 * 60. We will go through every single set of 60 characters
 # in the full_decoder function
-full_buffer = ciphertext
+full_buffer = decode(ciphertext.hex(), "hex_codec")
+# print full_buffer in its byte format
+# print(full_buffer)
 
 
 def full_decoder(cipher: bytes) -> str:
     result = ""
-    for i in range(166):
-        buffer = cipher[120*i: 120*(i+1)]
-        for letter in ascii_letters:
-            # Generate a 68 byte long key cast to bytes
-            key = bytes(letter * 68, 'utf-8')
-            if scorer(str(xor_buffers(key, buffer))) > 1.5:
+    for i in range(327):
+        # Load lines from the encrypted file one after one using get_chars()
+        buffer = get_chars(full_buffer[61*i:])
+        for letter in ascii_uppercase:
+            # Generate a 60 byte long key cast to bytes to get the key
+            key = bytes(letter * 60, 'utf-8')
+            if scorer(str(xor_buffers(key, buffer))) > 1:
                 result = str(xor_buffers(key, buffer))
                 print(letter, ": ", result)
     return result
 
 
-print(full_decoder(full_buffer))
+# define a method that gets values until it finds a line break, the output should
+def get_chars(inp: bytes) -> bytes:
+    output: str = ""
+    for i in str(inp):
+        if i != '\\':
+            output += i
+        else:
+            return bytes(output, 'utf-8')[2:]
+    return bytes(output, 'utf-8')[2:]
 
+
+# print(get_chars(full_buffer))
+print(full_decoder(full_buffer))
 """
 Algorithm:
-- We go through every 120 characters in the file at a time because
+- We go through every 60 characters in the file at a time because
 1 character = 1 byte
 2 hex characters = 1 byte
 hence
