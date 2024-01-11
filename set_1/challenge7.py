cf1bb23ffc6adc2f9bac4f3567f03609e1a38d49
Ihab Bokal <bokalihab@gmail.com>
2024-01-11 23:29:41 +0100

    Write solution's skeleton
    Code needs debugging to work

--- set_1/challenge7.py
+++ set_1/challenge7.py
@@ -1,4 +1,6 @@
 from base64 import b64decode
+from challenge2_solution import xor_buffers
+
 
 # 7.txt has been b64 encoded and encrypted with AES-128 in ECB mode under the key "YELLOW SUBMARINE"
 with open("7.txt", "r") as file:
@@ -17,5 +19,28 @@ you just break pt into blocks to cipher it to vice versa
 """
 
 
+# Return a list of blocks where each element is of length 16bytes
+def break_blocks(ct: bytes) -> list[bytes]:
+    blocks: list[bytes] = []
+
+    for i in range(len(ct) // 16):
+        blocks.append(ct[16*i: 16*(i+1)])
+
+    return blocks
+
+
+def break_aes_ecb(ct_blocks: list[bytes], k: bytes) -> bytes:
+    pt: bytes = b""
+    pt_part: bytes
+
+    for block in ct_blocks:
+        pt_part = xor_buffers(block, k)
+        pt += pt_part
+
+    return pt
+
+
 if __name__ == "__main__":
-    print(ciphered_file)
+    cipherBlocks = break_blocks(ciphered_file)
+    key: bytes = b"YELLOW SUBMARINE"
+    print(break_aes_ecb(cipherBlocks, key))
