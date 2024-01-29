a3465148b5adbb47f5feec1fb01863a43c39ec1c
Ihab Bokal <bokalihab@gmail.com>
2024-01-29 15:40:16 +0100

    Implement AES in CBC mode
    I need to test with openSSL

--- set_2/implement_cbc_10.py
+++ set_2/implement_cbc_10.py
@@ -2,6 +2,8 @@ from Crypto.Cipher import AES
 from set_1.challenge2_solution import xor_buffers
 from pkcs7_09 import pkcs7
 
+iv = b"fake 0th ciphertext block"
+
 
 def aes_ecb_enc(key: bytes, plaintext: bytes) -> bytes:
     # Create an AES object which will encrypt and decrypt the text we provide it with
@@ -10,15 +12,20 @@ def aes_ecb_enc(key: bytes, plaintext: bytes) -> bytes:
 
 
 def aes_cbc_enc(key: bytes, plaintext: bytes) -> bytes:
-    ct = b""
-    cipher_to_xor = iv
-    # We pad the plaintext
+    ct: bytes = b""
+    cipher_to_xor: bytes = iv
+    xored_pt: bytes
+    # Pad the plaintext so it is divisible by 16
+    plaintext = pkcs7(plaintext, len(plaintext) + len(plaintext) % 16)
+    for i in range(len(plaintext) // 16):
+        # XOR with previous ciphered block
+        xored_pt = xor_buffers(cipher_to_xor, plaintext[16*i: 16*(i+1)])
+        # Encrypt using the ECB method
+        cipher_to_xor += aes_ecb_enc(key, xored_pt)
+        ct += aes_ecb_enc(key, xored_pt)
     return ct
 
 
-iv = "fake 0th ciphertext block"
-
-
 if __name__ == "__main__":
-    print(aes_ecb_enc(b"YELLOW SUBMARINE", pkcs7(b"plaintext", 16)))
-
+    enc_key = b"YELLOW SUBMARINE"
+    print(aes_cbc_enc(enc_key, b"plaintext something to test shit yeah man, lkife be hhittin real good these days"))
