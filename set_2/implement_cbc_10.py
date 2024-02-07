0142db3dc83dfd4cfa0944cdab45f3fa31e8a549
Ihab Bokal <bokalihab@gmail.com>
2024-02-07 20:58:56 +0100

    Decrypt the file from cryptopals chall
    It was in base64

--- set_2/implement_cbc_10.py
+++ set_2/implement_cbc_10.py
@@ -2,6 +2,7 @@ from Crypto.Cipher import AES
 from set_1.challenge2_solution import xor_buffers
 from pkcs7_09 import pkcs7
 from set_1.challenge7_solution import aes_ecb_dec
+from base64 import b64decode
 
 iv = b"fake 0th ciphertext block"
 
@@ -12,9 +13,9 @@ def aes_ecb_enc(key: bytes, plaintext: bytes) -> bytes:
     return cipher.encrypt(plaintext)
 
 
-def aes_cbc_enc(key: bytes, plaintext: bytes) -> bytes:
+def aes_cbc_enc(key: bytes, plaintext: bytes, init_v: bytes) -> bytes:
     ct: bytes = b""
-    cipher_to_xor: bytes = iv
+    cipher_to_xor: bytes = init_v[:16]
     # Pad the plaintext so it is divisible by 16
     plaintext = pkcs7(plaintext, len(plaintext) + len(plaintext) % 16)
     for i in range(len(plaintext) // 16):
@@ -33,7 +34,7 @@ with open("10.txt", "r") as file:
 
 
 def aes_cbc_dec(key: bytes, ciphertext: bytes, init_v: bytes) -> bytes:
-    cipher_to_xor: bytes = init_v
+    cipher_to_xor: bytes = init_v[:16]
     pt: bytes = b""
     for i in range(len(ciphertext) // 16):
         # Decrypt the AES ECB block
@@ -43,29 +44,14 @@ def aes_cbc_dec(key: bytes, ciphertext: bytes, init_v: bytes) -> bytes:
         # Append the result to the plaintext
         pt += xored_block
         # Update the IV for the next iteration
-        cipher_to_xor = ciphertext[16 * i: 16 * (i + 1)]
-    return pt
-
-
-def aes_cbc_dec_gpt(key: bytes, ciphertext: bytes, init_v: bytes) -> bytes:
-    cipher_to_xor: bytes = init_v
-    pt: bytes = b""
-    for i in range(len(ciphertext) // 16):
-        # Decrypt the AES ECB block
-        decrypted_block: bytes = aes_ecb_dec(key, ciphertext[16 * i: 16 * (i + 1)])
-        # XOR with the IV (or previous ciphertext block for subsequent blocks)
-        xored_block: bytes = xor_buffers(decrypted_block, cipher_to_xor)
-        # Append the result to the plaintext
-        pt += xored_block
-        # Update the IV for the next iteration
-        cipher_to_xor = ciphertext[16 * i: 16 * (i + 1)]  # Use current ciphertext block instead of next one
+        cipher_to_xor = ciphertext[16 * i: 16 * (i+1)]
     return pt
 
 
 if __name__ == "__main__":
     enc_key = b"YELLOW SUBMARINE"
-    ciphrat: bytes = aes_cbc_enc(enc_key, b"plaintext something to test stuff yeah man, life be hitin real good these days")
-    print(ciphrat)
-    print(aes_cbc_dec_gpt(enc_key, ciphrat, iv))
-    # print(aes_cbc_dec(enc_key, cf, bytes([0]*16)))
+    ciphrat: bytes = aes_cbc_enc(enc_key, b"plaintext something to test stuff yeah man, life be hitin real good these days", iv)
+    print(aes_cbc_dec(enc_key, ciphrat, iv))
+    cipher: bytes = b64decode(cf)
+    print(aes_cbc_dec(enc_key, cipher, bytes([0]*16)))
     # print(bytes([0]*16))
